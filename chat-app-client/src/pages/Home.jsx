d7ced531515002d29149dbc3516eec4618ce5af8
hridayroy19 <hrhridoyroy503@gmail.com>
2025-07-02 17:40:19 +0600

    design chatmessgae

--- chat-app-client/src/pages/Home.jsx
+++ chat-app-client/src/pages/Home.jsx
@@ -8,15 +8,15 @@ const Home = () => {
   return (
     <div className="border  w-full h-screen sm:px-[15%] sm:py-[5%] ">
       <div
-        className={`backdrop-blur-xl border-2 boder-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative  ${
-          selectedUser
-            ? "md:grid-cols-[1fr_1.5fr_fr] xl:grid-cols-[1fr_2fr_fr]"
-            : "md:grid-cols-2"
-        } `}
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${
+    selectedUser
+      ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+      : "md:grid-cols-2"
+  }`}
       >
-        <Sidebar />
-        <ChatContainer />
-        <RightSideBar />
+        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <RightSideBar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       </div>
     </div>
   );
